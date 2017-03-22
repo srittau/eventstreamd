d283e02d75e438a188cb29ae04368f848c65927d
Sebastian Rittau <srittau@rittau.biz>
2017-03-22 19:17:46 +0100

    add support for a configuration file
    Closes #1

--- evtstrd/cmdargs.py
+++ evtstrd/cmdargs.py
@@ -6,17 +6,22 @@ from evtstrd.config import *
 def parse_command_line():
     parser = argparse.ArgumentParser(
         description="A simple event stream server.")
-    parser.add_argument(
-        "-s", "--socket", default=SOCKET_NAME, help="socket file")
-    parser.add_argument("--ssl-key", default=KEY_FILE, help="SSL key file")
-    parser.add_argument(
-        "--ssl-cert", default=CERT_FILE, help="SSL certificate file")
-    parser.add_argument(
-        "-p", "--port", default=HTTP_PORT, help="HTTP port", type=int)
+    parser.add_argument("-c", "--config-file", help="configuration file")
+    parser.add_argument("-s", "--socket", help="socket file")
+    parser.add_argument("--ssl-key", help="SSL key file")
+    parser.add_argument("--ssl-cert", help="SSL certificate file")
+    parser.add_argument("-p", "--port", help="HTTP port", type=int)
     args = parser.parse_args()
-    config = Config()
-    config.socket_file = args.socket
-    config.key_file = args.ssl_key
-    config.cert_file = args.ssl_cert
-    config.http_port = args.port
+    if args.config_file is not None:
+        config = read_config(args.config_file)
+    else:
+        config = read_default_config()
+    if args.socket is not None:
+        config.socket_file = args.socket
+    if args.ssl_key is not None:
+        config.key_file = args.ssl_key
+    if args.ssl_cert is not None:
+        config.cert_file = args.ssl_cert
+    if args.port is not None:
+        config.http_port = args.port
     return config
--- evtstrd/config.py
+++ evtstrd/config.py
@@ -1,3 +1,8 @@
+import configparser
+
+
+DEFAULT_CONFIG = "/etc/eventstreamd.conf"
+
 SOCKET_NAME = "/var/run/eventstreamd.sock"
 HTTP_PORT = 8888
 
@@ -15,3 +20,26 @@ class Config:
         self.key_file = KEY_FILE
         self.http_port = HTTP_PORT
         self.ping_interval = PING_INTERVAL
+
+
+def read_config(filename):
+    config = Config()
+    parser = configparser.ConfigParser()
+    with open(filename, "r") as f:
+        parser.read_file(f)
+        config.socket_file = parser.get(
+            "General", "SocketFile", fallback=SOCKET_NAME)
+        config.cert_file = parser.get(
+            "General", "SSLCertificateFile", fallback=CERT_FILE)
+        config.key_file = parser.get(
+            "General", "SSLKeyFile", fallback=KEY_FILE)
+        config.http_port = parser.getint(
+            "General", "HTTPPort", fallback=HTTP_PORT)
+    return config
+
+
+def read_default_config():
+    try:
+        return read_config(DEFAULT_CONFIG)
+    except FileNotFoundError:
+        return Config()
